22112ccea4f3138466c3cc1d437e7e87a8a231f4
settclow <ahmadrifa11620@gmail.com>
2023-06-20 15:23:55 +0700

    change to https

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require('express')
 const cors = require('cors')
 const endPoint = require('./app/route.js')
 const dotenv = require('dotenv')
+const https = require('https')
+const fs = require('fs')
 const app = express()
 dotenv.config()
 app.use(cors())
 app.use(express.json())
 const port = 4000
+const key = fs.readFileSync('./key_back_gpon.key', 'utf-8')
+const certificate = fs.readFileSync('./back_gpon.crt', 'utf-8')
+const credentials = { key: key, cert: certificate }
 app.use('/api', endPoint)
-
-app.listen(port, () => {
-    console.log(`server running on port ${port}`)
+const httpsServer = https.createServer(credentials, app)
+httpsServer.listen(port, () => {
+    console.log(`server running over https on port ${port}`)
 })
 
